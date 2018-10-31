64751d8acbde17685ed8da3b2ca757c09dc09606
Lesley Hogg <lesley@greenriver.org>
2018-10-31 15:37:38 -0400

    redis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  ENV["REDISTOGO_URL"] = 'redis://redistogo:46bba9cf2b1090c5ad892428f5931a9b@spinyfin.redistogo.com:11029/'
 end
