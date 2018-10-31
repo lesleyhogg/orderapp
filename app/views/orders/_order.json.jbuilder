6a9c4c0083d25e7e07d8a587090a3d628623b5aa
Lesley Hogg <lesley@greenriver.org>
2018-10-31 15:11:45 -0400

    Initial commit

---
+++ app/views/orders/_order.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! order, :id, :item, :created_at, :updated_at
+json.url order_url(order, format: :json)
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :orders
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ActionCable.server, at: '/cable'
+end
